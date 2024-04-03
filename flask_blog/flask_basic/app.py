c56d4ee1d68481592ee70ade62bc724729557957
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-04-03 18:40:15 -0400

    add debug mode

--- flask_blog/flask_basic/app.py
+++ flask_blog/flask_basic/app.py
@@ -12,7 +12,7 @@ def test():
 
 @app.route('/user/<user_id>')
 def userId(user_id):
-    return '<h1>Your User ID: {0}</h1>'.format(user_id)  # .format(user_id): ()の中の値が{}の中に入る
+    return '<h1>Your User ID: {0} {1} {2}</h1>'.format(user_id[0],user_id[1],user_id[2])  # .format(user_id): ()の中の値が{}の中に入る
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True) # debug=Trueにするとデバッグモードになって、エラーを表示してくれる
